54c1900a2a68c15618e9a8c6e86b217656868134
Ananya54321 <ananyapappula521@gmail.com>
2025-04-06 09:49:55 +0530

    added evnts

--- client/src/app/events/page.jsx
+++ client/src/app/events/page.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import EventForm from "@/components/pages/events/EventForm";
 
 const page = () => {
-  return <div>events</div>;
+  return (
+    <div>
+      <EventForm />
+    </div>
+  );
 };
 
 export default page;
